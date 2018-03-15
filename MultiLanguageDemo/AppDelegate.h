a44c5094eb330e2ee1dac3296f7a3a7122ea8fb5
ZSZ1994 <962369884@qq.com>
2018-03-15 10:09:21 +0800

    Initial Commit

---
+++ MultiLanguageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiLanguageDemo
+//
+//  Created by 朱松泽 on 2018/3/15.
+//  Copyright © 2018年 朱松泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
